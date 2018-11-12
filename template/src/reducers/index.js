ea2ee3276bee4db8726382c5b4cae41cd5eee9c2
BearGS <alexbear33821@gmail.com>
2018-11-12 14:51:03 +0800

    init

--- template/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-exports.__esModule = true
-const redux_1 = require('redux')
-const todos_1 = require('./todos')
-
-const reducers = redux_1.combineReducers({ todos: todos_1['default'] })
-exports['default'] = reducers
--- template/src/store/configureStore.prod.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-exports.__esModule = true
-const redux_1 = require('redux')
-const redux_thunk_1 = require('redux-thunk')
-const reducers_1 = require('../reducers')
-
-const configureProdStore = function (preloadedState) { return redux_1.createStore(reducers_1['default'], preloadedState, redux_1.applyMiddleware(redux_thunk_1['default'])) }
-exports['default'] = configureProdStore
